32414653f1a1cd163bc082f9f659b7d47d269048
Luke Waring <lukewaring@gmail.com>
2019-10-17 15:15:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -34,9 +34,9 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name][attribute] = []
         end
         pigeon_list[name][attribute].push(sub_attribute.to_s)
-        binding.pry
       end
     end
   end
   pigeon_list
+  binding.pry
 end
